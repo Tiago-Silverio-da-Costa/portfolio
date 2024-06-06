111ff286b99fb62235c45a542c94084d0889d41a
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2024-06-06 11:16:41 -0300

    right compoent

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Hero from "@/components/hero";
+import HeroOld from "@/components/hero-old";
 
 export default function Home() {
   return (
-    // <HeroOld />
-    <Hero />
+    <HeroOld />
+    // <Hero />
 
   );
 }
