d01a580531dd595319482eddae50d7c9023c88ea
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2024-06-13 16:30:53 -0300

    url security improved

--- components/commom/schemaproject.ts
+++ components/commom/schemaproject.ts
@@ -4,11 +4,11 @@ export const ProjectSchema = z.object({
     id: z.number().optional(),
     name: z.string().min(1, {message: "Required Field"}),
     description: z.string().min(1, {message: "Required Field"}),
-    image_url: z.string().min(1, {message: "Required Field"}),
-    gif_url: z.string().min(1, {message: "Required Field"}),
-    video_url: z.string().min(1, {message: "Required Field"}),
+    image_url: z.string().url().startsWith("https://i.imgur.com/").min(1, {message: "Required Field"}),
+    gif_url: z.string().url().startsWith("https://i.imgur.com/").min(1, {message: "Required Field"}),
+    video_url: z.string().url().startsWith("https://www.youtube.com/").min(1, {message: "Required Field"}),
     programming_language: z.string().min(1, {message: "Required Field"}),
-    repo_url: z.string().min(1, {message: "Required Field"}),
-    project_url: z.string().min(1, {message: "Required Field"})
+    repo_url: z.string().url().startsWith("https://github.com/").min(1, {message: "Required Field"}),
+    project_url: z.string().url().startsWith("https://").min(1, {message: "Required Field"})
 })
 export type TCreateProject = Zod.TypeOf<typeof ProjectSchema>
