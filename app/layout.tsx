f6f94c22f13565701508da3096a78c1d9d259ec2
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2024-07-03 18:02:14 -0300

    try tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "@/styles/globals.css";
 import StyledComponentsRegistry from "@/lib/styledRegistry";
 import localFont from "next/font/local";
+import Script from "next/script";
 
 const segoe = localFont({
 	src: [
@@ -64,8 +65,23 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="pt-BR">
+			<Script dangerouslySetInnerHTML={{
+				__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-K6NTFHG7');`
+			}} />
 			<body className={segoe.className}>
+				<noscript
+					dangerouslySetInnerHTML={{
+						__html: `
+						<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K6NTFHG7"
+						height="0" width="0" style="display:none;visibility:hidden"></iframe>
+					`,
+					}}
+				/>
 				<StyledComponentsRegistry>
 					{children}
 				</StyledComponentsRegistry>
