5c93803c2e95d98fa415f32f7d71b73bdf39f2d9
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2024-07-13 10:17:42 -0300

    adj

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -12,7 +12,7 @@ export default function Projects() {
             animate={{ scale: 1 }}
             transition={{ delay: 0.5, duration: 0.5 }}
             id="projects"
-            className="mx-auto w-5/6 max-w-5xl flex flex-col items-center md:items-start justify-start py-8 h-screen">
+            className="mx-auto w-5/6 max-w-5xl flex flex-col items-center md:items-start justify-start py-8">
             <h1 className="text-2xl md:text-3xl font-bold text-textTitle">Projetos: Uma vitrine de realizações</h1>
             <Repos />
         </motion.section>
