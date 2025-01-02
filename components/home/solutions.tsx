bb4350c15528b29428bffcac350db6aa04d46ce1
Tiago-Silverio-da-Costa <tiagosilveriodacosta@gmail.com>
2025-01-02 17:23:49 -0300

    feat: solutions bilinguals

--- components/home/solutions.tsx
+++ components/home/solutions.tsx
@@ -1,22 +1,27 @@
+"use client"
+
 import Image from "next/image";
+import { useLanguageStore } from "./context/languageContext";
 
 export default function Solutions() {
+    const { texts } = useLanguageStore();
+
     return (
         <section className="flex flex-col items-center justify-center py-32">
             <div className="mx-auto w-5/6 max-w-5xl">
 
                 <h1 className="text-2xl md:text-3xl font-bold text-textTitle">
-                    O que eu ofereço para o seu negócio
+                    {texts.solutions?.title || "O que eu ofereço para o seu negócio"}
                 </h1>
                 <div className=" flex flex-wrap gap-6 mt-8 justify-center items-stretch">
 
                     <div className="flex flex-col items-center shadow-md rounded-md px-6 py-4 bg-white">
                         <Image className="px-4 py-2 rounded-full bg-textTitle" src="/home/hero/landing.svg" alt="Landing page por Tiago Silverio Programador" width={100} height={100} />
                         <h2 className="font-semibold text-defaultText text-lg md:text-2xl tracking-wider">
-                            Landing Pages
+                            {texts.solutions?.landingPageTitle || "Landing Pages"}
                         </h2>
                         <p className="text-center text-lg mt-2 leading-6 tracking-tighter max-w-60">
-                            Converta visitantes em clientes potenciais de forma eficaz e direta.
+                            {texts.solutions?.landingPageDesc || "Converta visitantes em clientes potenciais de forma eficaz e direta."}
                         </p>
                     </div>
 
@@ -24,65 +29,63 @@ export default function Solutions() {
                     <div className="flex flex-col items-center shadow-md rounded-md px-6 py-4 bg-white">
                         <Image className="px-4 py-2 rounded-full bg-textTitle" src="/home/hero/ecommerce.svg" alt="E-commerce por Tiago Silverio Programador" width={100} height={100} />
                         <h2 className="font-semibold text-defaultText text-lg md:text-2xl tracking-wider">
-                            Lojas Virtuais
+                            {texts.solutions?.ecommerceTitle || "Lojas Virtuais"}
                         </h2>
                         <p className="text-center text-lg mt-2 max-w-60 leading-6 tracking-tighter">
-                            Dê vida aos seus produtos online, facilitando compras seguras e práticas para seus clientes.
+                            {texts.solutions?.ecommerceDesc || "Dê vida aos seus produtos online, facilitando compras seguras e práticas para seus clientes."}
                         </p>
                     </div>
 
                     <div className="flex flex-col items-center shadow-md rounded-md px-6 py-4 bg-white">
                         <Image className="px-4 py-2 rounded-full bg-textTitle" src="/home/hero/institutions.svg" alt="Site Institucional por Tiago Silverio Programador" width={100} height={100} />
                         <h2 className="font-semibold text-defaultText text-lg md:text-2xl tracking-wider">
-                            Sites Institucionais
+                            {texts.solutions?.institutionalWebsitesTitle || "Sites Institucionais"}
                         </h2>
                         <p className="text-center text-lg mt-2 max-w-60 leading-6 tracking-tighter">
-                            Apresente sua empresa, produtos e serviços, além de fortalecer sua marca e conquistar novos clientes.
+                            {texts.solutions?.institutionalWebsitesDesc || "Apresente sua empresa, produtos e serviços, além de fortalecer sua marca e conquistar novos clientes."}
                         </p>
                     </div>
 
                     <div className="flex flex-col items-center shadow-md rounded-md px-6 py-4 bg-white">
                         <Image className="px-4 py-2 rounded-full bg-textTitle" src="/home/hero/websystem.svg" alt="Sistema Web por Tiago Silverio Programador" width={100} height={100} />
                         <h2 className="font-semibold text-defaultText text-lg md:text-2xl tracking-wider">
-                            Sistemas Web
+                            {texts.solutions?.webSystemsTitle || "Sistemas Web"}
                         </h2>
                         <p className="text-center text-lg mt-2 leading-6 tracking-tighter max-w-60">
-                            Facilite suas operações, como vendas, gestão de clientes e controle de estoque, tudo integrado e seguro
+                            {texts.solutions?.webSystemsDesc || "Facilite suas operações, como vendas, gestão de clientes e controle de estoque, tudo integrado e seguro"}
                         </p>
                     </div>
 
                     <div className="flex flex-col items-center shadow-md rounded-md px-6 py-4 bg-white">
                         <Image className="px-4 py-3 rounded-full bg-textTitle" src="/home/hero/app.svg" alt="Aplicativo por Tiago Silverio Programador" width={80} height={100} />
                         <h2 className="font-semibold text-defaultText text-lg md:text-2xl tracking-wider">
-                            App
+                            {texts.solutions?.AppTitle || "Aplicativo"}
                         </h2>
                         <p className="text-center text-lg mt-2 leading-6 tracking-tighter max-w-60">
-                            Desenvolvemos aplicativos personalizados que conectam, engajam e transformam.
+                            {texts.solutions?.AppDesc || "Desenvolvemos aplicativos personalizados que conectam, engajam e transformam."}
                         </p>
                     </div>
 
                     <div className="flex flex-col items-center shadow-md rounded-md px-6 py-4 bg-white">
                         <Image className="px-4 py-3 rounded-full bg-textTitle" src="/home/hero/seo.svg" alt="Estratégia SEO por Tiago Silverio Programador" width={80} height={100} />
                         <h2 className="font-semibold text-defaultText text-lg md:text-2xl tracking-wider">
-                            Estratégias de SEO
+                            {texts.solutions?.seoTitle || "Estratégias de SEO"}
                         </h2>
                         <p className="text-center text-lg mt-2 leading-6 tracking-tighter max-w-60">
-                            Aumente a visibilidade do seu site e atraia mais tráfego qualificado.
+                            {texts.solutions?.seoDesc || "Aumente a visibilidade do seu site e atraia mais tráfego qualificado."}
                         </p>
                     </div>
 
                     <div className="flex flex-col items-center  shadow-md rounded-md px-6 py-4 bg-white">
                         <Image className="px-4 py-3 rounded-full bg-textTitle" src="/home/hero/fix.svg" alt="Manutenção por Tiago Silverio Programador" width={80} height={100} />
                         <h2 className="font-semibold text-defaultText text-lg md:text-2xl tracking-wider">
-                            Suporte
+                            {texts.solutions?.suportTitle || "Suporte"}
                         </h2>
                         <p className="text-center text-lg mt-2 leading-6 tracking-tighter max-w-60">
-                            Garanta que seu site permaneça atualizado, seguro e funcional
+                            {texts.solutions?.suportDesc || "Garanta que seu site permaneça atualizado, seguro e funcional"}
                         </p>
                     </div>
-
                 </div>
-
             </div>
         </section>
     )
